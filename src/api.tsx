78a27ee1fdcccfcd8bc3ad64c9a584afb537a0b1
Aboa123 <wlxm9999@gmail.com>
2021-12-01 23:45:17 +0900

    fetchCoinHistory change startDate length

--- src/api.tsx
+++ src/api.tsx
@@ -15,7 +15,7 @@ export const fetchCoinTicker = (coinId: string) => {
 
 export const fetchCoinHistory = (coinId: string) => {
   const endDate = Math.floor(Date.now() / 1000);
-  const startDate = endDate - 60 * 60 * 24 * 7 * 2;
+  const startDate = endDate - 60 * 60 * 24 * 7 * 4;
 
   return fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate }`).then(res => res.json());
 }
