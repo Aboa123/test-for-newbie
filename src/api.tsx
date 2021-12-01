9ad7facbedf8ba93f923653e5b951012283683eb
Aboa123 <wlxm9999@gmail.com>
2021-12-01 22:39:28 +0900

    add fetchCoinHistory on api.tsx

--- src/api.tsx
+++ src/api.tsx
@@ -12,3 +12,10 @@ export const fetchCoinInfo = (coinId: string) => {
 export const fetchCoinTicker = (coinId: string) => {
   return fetch(`${BASE_URL}/tickers/${coinId}`).then(res => res.json());
 }
+
+export const fetchCoinHistory = (coinId: string) => {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7 * 2;
+
+  return fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate }`).then(res => res.json());
+}
